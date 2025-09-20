bf82b7aabc070426d0dd1741e14deb42c82a5435
Tetiana Boiar <taniavasylchyk@gmail.com>
2025-09-20 13:29:15 +0300

    add basic layout with mock todos (tabs, checkboxes, delete button)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { TodoListComponent } from './pages/todo-list/todo-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [MatToolbarModule, TodoListComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular17-todo-app';
-}
+export class AppComponent {}
--- src/app/pages/todo-list/todo-list.component.ts
+++ src/app/pages/todo-list/todo-list.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Todo } from '../../models/todo.model';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatListModule } from '@angular/material/list';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTabsModule,
+    MatListModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    FormsModule
+  ],
   templateUrl: './todo-list.component.html',
-  styleUrl: './todo-list.component.scss'
+  styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent {
-
+  todos = signal<Todo[]>([
+    {
+      id: '1',
+      description: 'Complete the Angular 17 Todo App',
+      creationDate: '2025-09-20',
+      dueDate: '2025-09-22',
+      completed: false
+    },
+    {
+      id: '2',
+      description: 'Prepare the first part of theory',
+      creationDate: '2025-09-20',
+      dueDate: '2025-09-22',
+      completed: true,
+      completionDate: '2025-09-21'
+    },
+    {
+      id: '3',
+      description: 'Prepare the second part of theory',
+      creationDate: '2025-09-20',
+      dueDate: '2025-10-06',
+      completed: false
+    }
+  ]);
 }
